338dc0a7478878b4265ca7554093dedefa838921
AceJuno <kamcio225@gmail.com>
2017-01-29 13:10:27 +0100

    Update ball.js

--- Bouncing balls/ball.js
+++ Bouncing balls/ball.js
@@ -1,19 +1,19 @@
 function ball() {
-    var   x = constrain(mouseX,this.r/2,width -this.r/2);
-    var   y = constrain(mouseY,this.r/2,height-this.r/2);
+    var x = constrain(mouseX, this.r / 2, width - this.r / 2);
+    var y = constrain(mouseY, this.r / 2, height - this.r / 2);
     this.x = mouseX;
     this.y = mouseY;
-    this.r = 30;
+    this.r = 50;
     this.xspeed = random(-3, 3);
     this.yspeed = random(-3, 3);
-
+    constrain
     this.make = function() {
-      noStroke();
-      fill(0,0,0,150)
-        ellipse(this.x-1, this.y-1, this.r-5, this.r-5);
-        fill(113,161,208,100)
+        noStroke();
+        fill(0, 0, 0, 150)
+        ellipse(this.x - 1, this.y - 1, this.r - 5, this.r - 5);
+        fill(113, 161, 208, 100)
         ellipse(this.x, this.y, this.r, this.r);
-        fill(113,208,208,100)
+        fill(113, 208, 208, 100)
         ellipse(this.x, this.y, this.r, this.r);
     };
 
@@ -23,14 +23,14 @@ function ball() {
     };
 
     this.border = function(other) {
-        if (this.x - this.r/2 < 0) {
-            this.xspeed = random(0, 3);
-        } else if (this.y - this.r/2 < 0) {
-            this.yspeed = random(0, 3);
-        } else if (this.x + this.r/25 > width) {
-            this.xspeed = random(-3, 0);
-        } else if (this.y + this.r/2 > height) {
-            this.yspeed = random(-3, 0);
+        if (this.x - this.r / 2 < 0) {
+            this.xspeed = -this.xspeed;
+        } else if (this.y - this.r / 2 < 0) {
+            this.yspeed = -this.yspeed;
+        } else if (this.x + this.r / 2 > width) {
+            this.xspeed = -this.xspeed;
+        } else if (this.y + this.r / 2 > height) {
+            this.yspeed = -this.yspeed;
         }
     };
 
@@ -41,20 +41,20 @@ function ball() {
             return false;
         }
     };
-this.vel= function(other){
-  var velmemx = other.xspeed;
-  var velmemy = other.yspeed;
+    this.vel = function(other) {
+        var velmemx = other.xspeed;
+        var velmemy = other.yspeed;
 
-    other.xspeed = this.xspeed;
-    other.yspeed = this.yspeed;
-    this.xspeed = velmemx;
-    this.yspeed = velmemy;
+        other.xspeed = this.xspeed;
+        other.yspeed = this.yspeed;
+        this.xspeed = velmemx;
+        this.yspeed = velmemy;
 
 
 
-}
-this.offvel= function(other){
-  this.xspeed = -other.xspeed;
-  this.yspeed = -other.yspeed;
-}
+    }
+    this.offvel = function(other) {
+        this.xspeed = -other.xspeed;
+        this.yspeed = -other.yspeed;
+    }
 };
